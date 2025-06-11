ca1477ff2f6d0bfda12d20e1f1bf3339586772f0
whokartikgambhir <gambhirkartik6@gmail.com>
2025-06-11 17:38:42 +0530

    changes

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,12 @@
 // internal dependencies
 import { connectMongo } from "./config";
+import logger from "./logger";
 
 /**
  * Method to establish mongodb connection
  */
 export async function connect() {
+    logger.info("NODE_ENV => ", process.env.NODE_ENV);
     if(!process.env.NODE_ENV) {
         await connectMongo();
     } else {
