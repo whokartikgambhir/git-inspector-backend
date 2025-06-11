35674f477ea5530bcb9c7cf9ec55c0264773ba60
whokartikgambhir <gambhirkartik6@gmail.com>
2025-06-11 18:17:52 +0530

    removed-condition-for-db-connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,9 @@
 // internal dependencies
 import { connectMongo } from "./config";
-import logger from "./logger";
 
 /**
  * Method to establish mongodb connection
  */
 export async function connect() {
-    logger.info("NODE_ENV => ", process.env.NODE_ENV);
-    if(!process.env.NODE_ENV) {
-        await connectMongo();
-    } else {
-        // await connectDocumentDB();
-    }
+  await connectMongo();
 }
